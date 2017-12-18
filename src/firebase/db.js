4ce1eda30df62f917718dacc9dcc1bacb28c8bcf
Robin Wieruch <wrobin@gmx.net>
2017-12-18 11:26:46 +0800

    Revert "authorization: add role to user"
    This reverts commit 7ac9d1be04078c5c1e59cf4af1beba70bdc8a06f.

--- src/firebase/db.js
+++ src/firebase/db.js
@@ -2,11 +2,10 @@ import { db } from './firebase';
 
 // User API
 
-export const doCreateUser = (id, username, email, isAdmin) =>
+export const doCreateUser = (id, username, email) =>
   db.ref(`users/${id}`).set({
     username,
     email,
-    isAdmin,
   });
 
 export const onceGetUsers = () =>
