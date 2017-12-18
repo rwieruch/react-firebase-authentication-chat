7ac9d1be04078c5c1e59cf4af1beba70bdc8a06f
Robin Wieruch <wrobin@gmx.net>
2017-12-18 11:13:27 +0800

    authorization: add role to user

--- src/firebase/db.js
+++ src/firebase/db.js
@@ -2,10 +2,11 @@ import { db } from './firebase';
 
 // User API
 
-export const doCreateUser = (id, username, email) =>
+export const doCreateUser = (id, username, email, isAdmin) =>
   db.ref(`users/${id}`).set({
     username,
     email,
+    isAdmin,
   });
 
 export const onceGetUsers = () =>
